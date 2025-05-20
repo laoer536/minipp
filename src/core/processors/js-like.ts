8736a5f0851edfda9796536f19d8ad0fdc848acf
LiuJie <1547698569@qq.com>
2025-05-21 01:43:10 +0800

    fix: cli published run error

--- src/core/processors/js-like.ts
+++ src/core/processors/js-like.ts
@@ -1,5 +1,5 @@
 import { type ImportDeclaration, parse, type TsType, type JSXAttribute } from '@swc/core'
-import ASTVisitor from '@swc/core/Visitor.js'
+import ASTVisitor from '../visitor/Visitor'
 import { glob } from 'glob'
 import path from 'path'
 import fs from 'fs'
